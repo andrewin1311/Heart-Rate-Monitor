0b8a64181fc2cdf529dabab5e11455638a504d9a
mrmediocre83 <patrick.mcdonald001@umb.edu>
2025-04-09 21:12:53 -0400

    Create Clock object .h and .cpp file
    Created the Clock object with some prototype functions and a time block struct.  Thought I'd try to see if we could make it for generalized and made a template function for future things like step monitoring, oxygen sensors, etc,...  Also began the .cpp file.  I'll try and put the diffrent function on the Kanban board so folks can start doing little bits at a time.

---
+++ project/Clock.cpp
@@ -0,0 +1,10 @@
+#include "clock.h"
+#include <Aruduino.h>
+
+// Not certain if there is a Real-Time Clock (RTC) but I found
+// the libraries are:
+// #include <Wire.h>
+// #include <RTClib.h>
+//
+// To define our RTC object we can use:
+// RTC_DS3231 rtc;   (or something...)
