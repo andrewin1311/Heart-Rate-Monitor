1530d497b7f61b078b85b41955a2a4715c54449c
Charles J Quigley <Chasequigley9@gmail.com>
2025-04-29 10:19:47 -0400

    syncing

--- project/server.py
+++ project/server.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 import random # Simulate random BPM data (Fake data for testing)
+import serial
+import time
+import string
+
 
 app = Flask(__name__, static_folder='static_website') # Create a Flask app
+CORS(app)
 
 # Serve index.html from /static_website
 @app.route('/') # Serve the website
@@ -10,10 +16,24 @@ def serve_index():
 
 # Function name: get_bpm()
 # Purpose: To simulate the retrieval of heart rate data (BPM) from a device or database.
+
 @app.route('/bpm') # Serve the BPM data
 def get_bpm(): 
-    bpm = random.randint(60, 100)
+    ser = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=1)
+    time.sleep(2)
+    bpm = 0
+    try:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8', errors='ignore').strip()
+            bpm = line.strip(string.ascii_letters)
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        if ser.is_open:
+            ser.close()
+    print("getting value")
+    
     return jsonify({'bpm': bpm})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
